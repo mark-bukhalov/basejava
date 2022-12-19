3d6334b41fbf1e0876ac5924585719f43383470c
Бухалов Марк Васильевич <mbuhalov@msc.sigma-it.local>
2022-12-19 22:22:11 +0300

    ArrayStorage methods implementation

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -3,28 +3,48 @@
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
+    private int size;
 
     void clear() {
+        for (int i = 0; i < size; i++) {
+            storage[i] = null;
+        }
+        size = 0;
     }
 
     void save(Resume r) {
+        storage[size] = r;
+        size++;
     }
 
     Resume get(String uuid) {
+        for (int i = 0; i < size; i++) {
+            if (storage[i].uuid == uuid) {
+                return storage[i];
+            }
+        }
         return null;
     }
 
     void delete(String uuid) {
+        for (int i = 0; i < size; i++) {
+            if (storage[i].uuid == uuid) {
+                size--;
+                System.arraycopy(storage, i + 1, storage, i, size - i);
+            }
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        Resume[] storageCopy = new Resume[size];
+        System.arraycopy(storage, 0, storageCopy, 0, size);
+        return storageCopy;
     }
 
     int size() {
-        return 0;
+        return size;
     }
 }
