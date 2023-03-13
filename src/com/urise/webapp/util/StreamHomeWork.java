8754e035136af8af61ed756647f63ab9659daa46
mark-bukhalov <mark.bukhalov98@gmail.com>
2023-03-13 19:33:40 +0300

    Lesson12 HW edit1

--- src/com/urise/webapp/util/StreamHomeWork.java
+++ src/com/urise/webapp/util/StreamHomeWork.java
@@ -2,8 +2,6 @@ package com.urise.webapp.util;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.TreeSet;
-import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class StreamHomeWork {
@@ -15,21 +13,24 @@ public class StreamHomeWork {
         list.add(1);
         list.add(1);
         list.add(1);
+        list.add(1);
         list.add(2);
         System.out.println(oddOrEven(list));
     }
 
     static int minValue(int[] values) {
-        return IntStream.of(values).boxed().collect(Collectors.toCollection(TreeSet::new)).
-                stream().reduce(0, (result, element) -> result * 10 + element);
+        return IntStream.of(values)
+                .distinct()
+                .sorted()
+                .reduce(0, (result, element) -> result * 10 + element);
     }
 
     static List<Integer> oddOrEven(List<Integer> integers) {
-        int sum = integers.stream().reduce(0, (result, element) -> result + element);
-        if (sum % 2 == 0) {
-            return integers.stream().filter(integer -> (integer % 2 != 0)).toList();
-        } else {
-            return integers.stream().filter(integer -> (integer % 2 == 0)).toList();
-        }
+        int sum = integers.stream()
+                .reduce(0, Integer::sum);
+        boolean isEven = sum % 2 == 0;
+        return integers.stream()
+                .filter(integer -> (integer % 2 != 0) == isEven)
+                .toList();
     }
 }
