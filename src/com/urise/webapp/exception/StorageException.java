33c00d2ec07872f95a6b6dc68765a6472afcd6fe
mark-bukhalov <mark.bukhalov98@gmail.com>
2023-03-15 20:12:02 +0300

    Lesson13 SqlStorage

--- src/com/urise/webapp/exception/StorageException.java
+++ src/com/urise/webapp/exception/StorageException.java
@@ -3,6 +3,15 @@ package com.urise.webapp.exception;
 public class StorageException extends RuntimeException {
     private final String uuid;
 
+
+    public StorageException(Exception e) {
+        this(e.getMessage(), e);
+    }
+
+    public StorageException(String message, Exception e) {
+        this(message, null, e);
+    }
+
     public StorageException(String message, String uuid) {
         super(message);
         this.uuid = uuid;
